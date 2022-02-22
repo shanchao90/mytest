38ad28be7da674cdd9aa2f2741145bced4f91d65
shanchao90 <shanchao90@163.com>
2022-02-23 00:45:05 +0800

    sock，class

--- server_main.cpp
+++ server_main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <mysql/mysql.h>
 
 using namespace std;
 
 class person
 {
 public:
+   void print_idsys();//打印ID
+   void print_idper();//打印身份证
+   void print_perName();//打印姓名
+   void print_all();//打印所有
+   void ins();//增
+   void del();//删
+   void upd();//改
+   void sel();//查
 
 private:
-int id_sys;
-int id_per;
-
+   int id_sys;//系统内ID
+   int per_id;//身份证
+   string perName;//姓名 
 };
 
 class casebase
@@ -22,13 +37,29 @@ private:
 
 };
 
-int main()
+int main(int argc,char *argv[])
 {
-   vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-   for (const string& word : msg)
-   {
-      cout << word << " ";
-   }
-   cout << endl;
+    //创建套接字
+    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //将套接字和IP、端口绑定
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;  //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
+    serv_addr.sin_port = htons(1234);  //端口
+    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    //进入监听状态，等待用户发起请求
+    listen(serv_sock, 20);
+    //接收客户端请求
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    //向客户端发送数据
+    char str[] = "Hello World!";
+    write(clnt_sock, str, sizeof(str));
+   
+    //关闭套接字
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
 }
